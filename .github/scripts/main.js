249880896dc72b4194f715e1a51306f82b178a00
Mike Bruzina <mbruzina@newrelic.com>
2024-03-25 14:40:23 -0500

    updated for region-specific tests

--- .github/scripts/main.js
+++ .github/scripts/main.js
@@ -3,12 +3,13 @@ const {SQSClient, SendMessageCommand} = require('@aws-sdk/client-sqs')
 const {DynamoDBClient, QueryCommand} = require('@aws-sdk/client-dynamodb')
 const {unmarshall} = require('@aws-sdk/util-dynamodb')
 
-const AWS_REGION = process.env.AWS_REGION
-const SQS_URL = process.env.SQS_URL
-const DYNAMO_TABLE = process.env.DYNAMO_TABLE
+const region = process.argv(2)
+const AWS_REGION = process.env[`DEPLOYER_PLATFORM_${region}.AWS_REGION`]
+const SQS_URL = process.env[`DEPLOYER_PLATFORM_${region}.SQS_URL`]
+const DYNAMO_TABLE = process.envenv[`DEPLOYER_PLATFORM_${region}.DYNAMO_TABLE`]
 const AWS_CREDS = {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.envenv[`DEPLOYER_PLATFORM_${region}.AWS_ACCESS_KEY_ID`],
+    secretAccessKey: process.env[`DEPLOYER_PLATFORM_${region}.AWS_SECRET_ACCESS_KEY`]
 };
 
 const sqs = new SQSClient({region: AWS_REGION, credentials: AWS_CREDS})
